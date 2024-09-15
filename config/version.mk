c0smic-Lab/vendor_aosp

build: Move around some props

config/version.mk
@@ -11,5 +11,12 @@ CUSTOM_PLATFORM_VERSION := 14.0
 CUSTOM_VERSION := cAOSP_$(CUSTOM_BUILD)-$(CUSTOM_PLATFORM_VERSION)-$(CUSTOM_BUILD_DATE)
 CUSTOM_VERSION_PROP := fourteen
 
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    ro.custom.build.date=$(BUILD_DATE) \
+    ro.custom.device=$(CUSTOM_BUILD) \
+    ro.custom.fingerprint=$(ROM_FINGERPRINT) \
+    ro.custom.version=$(CUSTOM_VERSION) \
+    ro.modversion=$(CUSTOM_VERSION)
+
 # Signing
 -include vendor/lineage-priv/keys/keys.mk

pix106/android_vendor_pixelage

config/version.mk
@@ -12,5 +12,13 @@ PIXELAGE_PLATFORM_VERSION := 15.0
 
 PIXELAGE_VERSION := v$(PIXELAGE_DISPLAY_VERSION)-$(PIXELAGE_BUILD)-$(PIXELAGE_PLATFORM_VERSION)-$(PIXELAGE_BUILD_DATE)
 
+# Pixelage Platform Version
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    ro.pixelage.build.date=$(BUILD_DATE) \
+    ro.pixelage.device=$(PIXELAGE_BUILD) \
+    ro.pixelage.fingerprint=$(ROM_FINGERPRINT) \
+    ro.pixelage.version=$(PIXELAGE_VERSION) \
+    ro.modversion=$(PIXELAGE_VERSION)
+
 # Signing
 -include vendor/pixelage-priv/keys/keys.mk
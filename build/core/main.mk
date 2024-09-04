9ac345edc498a34faee471e8e08822d25576a9ed
COSMIC <rayavanindra@gmail.com>
2024-09-05 00:06:08 +0530

    build: Move around some props

--- build/core/main.mk
+++
@@ -1,2 +0,0 @@
-# Include Pixelage versions
-include $(TOPDIR)vendor/pixelage/build/core/main_version.mk
--- config/version.mk
+++ config/version.mk
@@ -12,5 +12,13 @@ PIXELAGE_PLATFORM_VERSION := 15.0
 
 PIXELAGE_VERSION := v$(PIXELAGE_DISPLAY_VERSION)-$(PIXELAGE_BUILD)-$(PIXELAGE_PLATFORM_VERSION)-$(PIXELAGE_BUILD_DATE)
 
+# Pixelage Platform Version
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    ro.pixelage.build.date=$(BUILD_DATE) \
+    ro.pixelage.device=$(PIXELAGE_BUILD) \
+    ro.pixelage.fingerprint=$(ROM_FINGERPRINT) \
+    ro.pixelage.version=$(PIXELAGE_VERSION) \
+    ro.modversion=$(PIXELAGE_VERSION)
+
 # Signing
 -include vendor/pixelage-priv/keys/keys.mk
