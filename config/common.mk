clarencekopitiam/android_vendor_lineage

vendor: Set Google Sans Text as the default fonts
Co-authored-by: Anay Wadhera <anay1018@gmail.com>
Change-Id: I1509e59f9a0a662a7ba8d73148411a1ce8be3991
Signed-off-by: clarencelol <clarencekuiek@proton.me>

config/common.mk
@@ -90,6 +90,9 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.biometrics.face.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.hardware.biometrics.face.xml
 endif
 
+# Fonts
+include vendor/lineage/config/fonts.mk
+
 # This is Lineage!
 PRODUCT_COPY_FILES += \
     vendor/lineage/config/permissions/org.lineageos.android.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/org.lineageos.android.xml

config/fonts.mk
@@ -1,0 +1,7 @@
+# Fonts
+PRODUCT_COPY_FILES += \
+    vendor/lineage/fonts/GoogleSans-Regular.ttf:$(TARGET_COPY_OUT_PRODUCT)/fonts/GoogleSans-Regular.ttf \
+    vendor/lineage/fonts/GoogleSans-Italic.ttf:$(TARGET_COPY_OUT_PRODUCT)/fonts/GoogleSans-Italic.ttf \
+    vendor/lineage/fonts/GoogleSansClock-Regular.ttf:$(TARGET_COPY_OUT_PRODUCT)/fonts/GoogleSansClock-Regular.ttf \
+    vendor/lineage/fonts/GoogleSansFlex-Regular.ttf:$(TARGET_COPY_OUT_PRODUCT)/fonts/GoogleSansFlex-Regular.ttf \
+    vendor/lineage/fonts/fonts_customization.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/fonts_customization.xml

pix106/android_vendor_pixelage

config/common.mk
@@ -201,6 +201,9 @@ CUSTOM_LOCALES += \
 # Bootanimation
 include vendor/pixelage/config/bootanimation.mk
 
+# Fonts
+include vendor/pixelage/config/fonts.mk
+
 # Version
 include vendor/pixelage/config/version.mk
 

config/fonts.mk
@@ -1,0 +1,7 @@
+# Fonts
+PRODUCT_COPY_FILES += \
+    vendor/pixelage/fonts/GoogleSans-Regular.ttf:$(TARGET_COPY_OUT_PRODUCT)/fonts/GoogleSans-Regular.ttf \
+    vendor/pixelage/fonts/GoogleSans-Italic.ttf:$(TARGET_COPY_OUT_PRODUCT)/fonts/GoogleSans-Italic.ttf \
+    vendor/pixelage/fonts/GoogleSansClock-Regular.ttf:$(TARGET_COPY_OUT_PRODUCT)/fonts/GoogleSansClock-Regular.ttf \
+    vendor/pixelage/fonts/GoogleSansFlex-Regular.ttf:$(TARGET_COPY_OUT_PRODUCT)/fonts/GoogleSansFlex-Regular.ttf \
+    vendor/pixelage/fonts/fonts_customization.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/fonts_customization.xml